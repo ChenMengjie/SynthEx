51eff453aa61fb1c45969b4c9ff939b9c20b6b50
ChenMengjie <chenmengjie87@gmail.com>
2017-02-25 20:57:55 -0600

    update user guide

--- R/createTargetBins.R
+++ R/createTargetBins.R
@@ -1,21 +1,21 @@
 createTargetBins <- function(Target, bin.size){
   options(scipen = 50)
   start <- floor(Target[, 2]/bin.size)*bin.size
-  end <- ceiling(Target[, 2]/bin.size)*bin.size
+  end <- ceiling(Target[, 3]/bin.size)*bin.size
   res <- cbind(Target, Target[, 1], start, end)
+  flag <- which(res[, 6] - res[, 5] != bin.size)
+  colnames(res) <- paste0("V", 1:6)
+
   all <- NULL
-  for(i in 1:nrow(res)){
-    if(res[i, 6] - res[i, 5] == bin.size){
-      all <- rbind(all, res)
-    } else {
-      atarget <- seq(res[i, 5], res[i, 6] - bin.size, bin.size)
-      len <- length(atarget)
-      all <- rbind(all, cbind(res[i, 1:4], atarget, atarget + bin.size))
-    }
+  for(i in flag){
+    atarget <- seq(res[i, 5], res[i, 6] - bin.size, bin.size)
+    len <- length(atarget)
+    all <- rbind(all, data.frame(res[i, 1:4], atarget, atarget + bin.size, row.names = NULL))
   }
-  all <- as.data.frame(all)
-  names(all) <- paste0("V", 1:6)
-  return(all)
+  colnames(all) <- paste0("V", 1:6)
+
+  final <- rbind(res[-flag, ], all)
+  return(final)
 }
 
 
